334812d218ab2296b935d160121a3f20413b6e0e
Miguel-prog1 <70441960+Miguel-prog1@users.noreply.github.com>
2026-09-20 12:27:46 -0300

    A punto de terminar el deiseño del primer page de reporte

--- Frontend/src/app/pages/reportes/reportes.component.ts
+++ Frontend/src/app/pages/reportes/reportes.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { IonButton, IonCard, IonCardContent, IonCardHeader, IonContent, IonHeader, IonTitle} from '@ionic/angular';
+import { IonButton, IonCard, IonCardContent, IonCardHeader, IonContent, IonFooter, IonHeader, IonNote, IonTitle} from '@ionic/angular';
 import { Report } from '../../models/reporte';
 import { IonCardTitle } from '@ionic/angular';
 @Component({
@@ -7,7 +7,7 @@ import { IonCardTitle } from '@ionic/angular';
   templateUrl: './reportes.component.html',
   standalone: true, 
   styleUrls: ['./reportes.component.scss'],
-  imports: [IonHeader, IonContent, IonCard,IonCardHeader, IonCardContent,IonContent,IonCardTitle,IonTitle,IonButton],
+  imports: [IonHeader, IonContent, IonCard,IonCardHeader, IonCardContent,IonContent,IonCardTitle,IonTitle,IonButton, IonNote, IonFooter],
 })
 export class ReportesComponent  implements OnInit {
 
